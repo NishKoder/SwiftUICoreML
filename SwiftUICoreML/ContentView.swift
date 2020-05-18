9347447e1a3916210183ff9c26fd368c76e092d2
Nishant Gupta <shub002gupta@gmail.com>
2020-05-18 22:21:34 +0530

    Initial Commit

---
+++ SwiftUICoreML/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICoreML
+//
+//  Created by Nishant Gupta on 18/05/20.
+//  Copyright © 2020 Nishant Gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
